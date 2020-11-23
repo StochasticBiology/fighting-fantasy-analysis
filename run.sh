9ea9808e9cbe5bbe5f97f49eedc443aa1df20d17
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2020-11-23 21:45:29 +0100

    Add files via upload

--- run.sh
+++ run.sh
@@ -4,9 +4,12 @@ gcc -o3 battlesim.c -lm -o battlesim.ce
 #gcc -o3 heuristic.c -lm -o heuristic.ce
 gcc -o3 simpleluck.c -lm -o simpleluck.ce
 gcc -o3 optimal-strategies.c -lm -o optimal-strategies.ce
-./battlesim.ce > b.out &
+./battlesim.ce > b.out 
 #./heuristic.ce > h.out &
-./simpleluck.ce > sl.out &
-./optimal-strategies.ce 0 > os0.out &
-./optimal-strategies.ce 1 > os1.out &
+./simpleluck.ce > sl.out 
+./optimal-strategies.ce 0 > os0.out 
+./optimal-strategies.ce 1 > os1.out 
 # these .out files get big (70M)
+
+R CMD BATCH rtree.R
+
