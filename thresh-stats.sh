cb63b1e3dcdf6c519b6e7ed5b2badfd062d9de2e
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2020-11-24 09:20:19 +0100

    Delete thresh-stats.sh

--- thresh-stats.sh
+++
@@ -1,3 +0,0 @@
-awk '{if($15 == 1 && $6 > $5+0.01) {n[$16]++; total++;}}END{for(i=1;i<=12;i++) print i, n[i]/total;}' heuristic.txt > thresh1.txt
-awk '{if($15 == 2 && $7 > $5+0.01) {n[$16]++; total++;}}END{for(i=1;i<=12;i++) print i, n[i]/total;}' heuristic.txt > thresh2.txt
-awk '{if($15 == 3 && $8 > $5+0.01) {n[$16]++; total++;}}END{for(i=1;i<=12;i++) print i, n[i]/total;}' heuristic.txt > thresh3.txt
